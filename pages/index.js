6ccd5a744eba0cadcc9dff78080beaee78663a8e
Helena Adelsten <helenaltv@gmail.com>
2024-02-24 17:59:10 +0100

    Update index.js Home sida
    Satt in Economy news som länkas till artikeln

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,30 @@
-const HomePage = () => {
+import Link from "next/link";
+
+export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <main className="flex-grow p-4">
-        <div className="grid grid-cols-3 gap-4">
-          {/* News articles will go here */}
-          <div className="bg-white shadow-md p-4">
-            <h2 className="text-xl font-bold">Article Title</h2>
-            <p className="text-gray-700">Article summary...</p>
+    <div>
+      <div className="text-blue-600 text-6xl flow-root m-4 gap-9">News:</div>
+      <div className="flex flex-col min-h-screen">
+        <main className="flex-grow p-4">
+          <div className="grid grid-cols-3 gap-4">
+            {/* News articles will go here */}
+            <div className="bg-white shadow-md p-4">
+              <h2 className="text-xl font-bold">Article Title</h2>
+              <p className="text-gray-700">Article summary...</p>
+            </div>
+            <div className="bg-white shadow-md p-4">
+              <h2 className="text-xl font-bold">Article Title</h2>
+              <p className="text-gray-700">Article summary...</p>
+            </div>
+            <div className="bg-white shadow-md p-4">
+              <h2 className="text-2xl">
+                <Link href={"/economy"}>Economy news</Link>
+              </h2>
+            </div>
+            {/* Repeat the above div for each article */}
           </div>
-          {/* Repeat the above div for each article */}
-        </div>
-      </main>
+        </main>
+      </div>
     </div>
   );
-};
-
-export default HomePage;
+}
